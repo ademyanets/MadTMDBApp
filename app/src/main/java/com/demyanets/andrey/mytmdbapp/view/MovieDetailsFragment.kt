5d3fd35affd9bf41f528ba8f59602cd1ed170707
Andrey <a.demyanets@mcplat.ru>
2021-08-23 16:09:09 +0300

    Use constraint layout guidlines and flow helpers to make grids and align controls. Add views programmatically

--- app/src/main/java/com/demyanets/andrey/mytmdbapp/view/MovieDetailsFragment.kt
+++ app/src/main/java/com/demyanets/andrey/mytmdbapp/view/MovieDetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.demyanets.andrey.mytmdbapp.view
 
+import android.app.ActionBar
+import android.graphics.Color
 import android.os.*
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Constraints
 import androidx.fragment.app.Fragment
 import coil.load
 import coil.transform.CircleCropTransformation
@@ -24,7 +27,11 @@ class MovieDetailsFragment: Fragment() {
     lateinit var text: TextView
     lateinit var title: TextView
     lateinit var image: ImageView
+//    lateinit var rating: RatingBar
+    lateinit var details: TextView
     lateinit var spinner: ProgressBar
+    lateinit var companiesFlow: Flow
+    lateinit var layout: ConstraintLayout
 
     lateinit var repository: TmdbRepository
 
@@ -53,17 +60,15 @@ class MovieDetailsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        layout = view.findViewById<ConstraintLayout>(R.id.movie_details_layout)
         title = view.findViewById<TextView>(R.id.movie_details_title)
         text = view.findViewById<TextView>(R.id.movie_details_review)
+        details = view.findViewById<TextView>(R.id.movie_details)
         image = view.findViewById<ImageView>(R.id.movie_details_image)
         spinner = view.findViewById<ProgressBar>(R.id.movie_details_spinner)
+        companiesFlow = view.findViewById<Flow>(R.id.flow)
+//        rating = view.findViewById<RatingBar>(R.id.movie_details_rating)
         spinner.visibility = View.VISIBLE
-
-        var params = this.image.layoutParams
-        val size = 2 * view.width / 3
-        params.width = size
-        params.height = size
-        image.layoutParams = params
     }
 
     private fun requestCompletion(result: RequestResult) {
@@ -73,12 +78,20 @@ class MovieDetailsFragment: Fragment() {
             is RequestResult.ObjSuccess<*> -> {
                 val movie = result.data as MovieDTO //TODO: FIXME:
                 title.setText(movie.title)
-                text.setText(movie.overview)
-                image.load("https://image.tmdb.org/t/p/original${movie.backdrop_path}") {//FIXME: add /configuration request
-                    crossfade(true)
-                    placeholder(R.drawable.ic_backstep_preview)
-                    transformations(CircleCropTransformation())
+                text.setText("${movie.overview}\n\n${movie.homepage}")
+                details.setText("${movie.genres[0].name}\nOverall: ${movie.vote_average}\n\nReleased ${movie.release_date}\n\nBudget ${movie.budget}$")
+                image.load("https://image.tmdb.org/t/p/original${movie.backdrop_path}") //FIXME: add /configuration request
+
+                var ids: Array<Int> = emptyArray()
+                movie.production_companies.forEachIndexed { index, comp ->
+                    var logo = ImageView(activity)
+                    logo.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, 150)//(ConstraintLayout.LayoutParams.WRAP_CONTENT,100)
+                    logo.load("https://image.tmdb.org/t/p/w500${comp.logo_path}")
+                    logo.id = comp.id //FIXME:
+                    ids += logo.id
+                    layout.addView(logo)
                 }
+                companiesFlow.setReferencedIds(ids.toIntArray())
 
                 spinner.visibility = View.GONE
                 Toast.makeText(activity, movie.title, Toast.LENGTH_LONG).show()
