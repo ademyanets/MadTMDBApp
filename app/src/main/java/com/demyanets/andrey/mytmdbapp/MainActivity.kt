b7e3ce10011f7a77f262497c484ec1677ee935f5
Andrey <ademyanets@gmail.com>
2021-09-01 23:39:39 +0300

    connecting searcView to search manager [WIP]

--- app/src/main/java/com/demyanets/andrey/mytmdbapp/MainActivity.kt
+++ app/src/main/java/com/demyanets/andrey/mytmdbapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.demyanets.andrey.mytmdbapp
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,28 +27,47 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.fragment_container, TopRatedFragment())
             }
         }
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+Log.d("GGG", "GGG ${intent}")
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d("GGG", "GGG ${intent}")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.default_menu, menu)
-        val searchItem: MenuItem? = menu?.findItem(R.id.action_search)
-        val searchView = searchItem?.actionView as SearchView
+//        val searchItem: MenuItem? = menu?.findItem(R.id.action_search)
+//        val searchView = searchItem?.actionView as SearchView
+//
+//        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                searchView.clearFocus()
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                if (newText == null || newText.isEmpty()) {
+//                  //?
+//                } else {
+//                   //?
+//                }
+//                return false
+//            }
+//        })
 
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchView.clearFocus()
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText == null || newText.isEmpty()) {
-                  //?
-                } else {
-                   //?
-                }
-                return false
-            }
-        })
+    // Get the SearchView and set the searchable configuration
+    val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+    (menu?.findItem(R.id.action_search)?.actionView as SearchView).apply {
+        // Assumes current activity is the searchable activity
+        setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        setIconifiedByDefault(true) // Do not iconify the widget; expand it by default
+    }
 
         return super.onCreateOptionsMenu(menu)
     }
